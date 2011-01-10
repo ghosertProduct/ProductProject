14cac34592309332f48eeffbbaf67a5d3f292679
jiawzhang <ghosert@gmail.com>
2011-01-10 23:58:30 +0800

    update deploy script for realrecorder.net

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,8 @@
 # deploy steps here: copy env-conf/* to nginx and uwsgi
 # deploy html/py codes to nginx/html
 
+sudo kill -QUIT `cat /usr/local/nginx/logs/nginx.pid`
+sudo rm /usr/local/nginx/html/realrecorder.net -rf
+sudo cp realrecorder.net /usr/local/nginx/html/ -r
+sudo cp env-conf/nginx.conf /usr/local/nginx/conf/
+sudo nginx
