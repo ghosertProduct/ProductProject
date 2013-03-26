2af3e3577b45032f8ed26390c5e837d424c35cb9
ghosert <ghosert@gmail.com>
2013-03-26 20:29:58 +0800

    update models.py

--- zuoye/ZuoYeProject/zuoyeproject/models.py
+++ zuoye/ZuoYeProject/zuoyeproject/models.py
@@ -16,8 +16,8 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(Text, unique=True)
     value = Column(Integer)
